d3a4410aa1f3d83fa14df282b4c5010d447d23a9
kinsin5 <jakobgrob9@gmail.com>
2026-02-25 22:04:47 +0100

    prototyping with sqlite db

---
+++ schema.sql
@@ -0,0 +1,51 @@
+CREATE TABLE api_call (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    call_timestamp TEXT DEFAULT (datetime('now')),
+    status TEXT,
+    cities_fetched INTEGER
+);
+
+CREATE TABLE city (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    latitude REAL,
+    longitude REAL,
+    voivodeship TEXT,
+    elevation INTEGER
+
+);
+
+CREATE TABLE weather_current (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    api_call_id  INTEGER REFERENCES api_call(id),
+    city_id  INTEGER REFERENCES city(id),
+    time  TEXT,
+    interval_seconds INTEGER,
+    temperature_2m REAL,
+    relative_humidity_2m REAL,
+    apparent_temperature REAL,
+    is_day INTEGER,
+    precipitation REAL,
+    weather_code INTEGER,
+    cloud_cover INTEGER,
+    pressure_msl REAL,
+    surface_pressure REAL,
+    wind_speed_10m REAL
+);
+
+CREATE TABLE weather_forecast(
+    id  INTEGER PRIMARY KEY AUTOINCREMENT,
+    api_call_id  INTEGER REFERENCES api_call(id),
+    city_id  INTEGER REFERENCES city(id),
+    time  TEXT,
+    temperature_2m REAL,
+    relative_humidity_2m REAL,
+    apparent_temperature REAL,
+    precipitation_probability INTEGER,
+    precipitation REAL,
+    weather_code INTEGER,
+    cloud_cover INTEGER,
+    pressure_msl REAL,
+    surface_pressure REAL,
+    wind_speed_10m REAL
+);
