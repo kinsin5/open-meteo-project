0c24683f982a75546e20a87566c69b54b45580af
kinsin5 <jakobgrob9@gmail.com>
2026-02-27 22:54:07 +0100

    xd

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import streamlit as st
 from numpy.random import default_rng as rng
+import sqlite3
 
-df = pd.DataFrame(
-    rng(0).standard_normal((1000, 2)) / [50, 50] + [37.76, -122.4],
-    columns=["lat", "lon"],
-)
+conn = sqlite3.connect('db/weather.db')
+
+c = conn.cursor()
+
+def get_cities() -> pd.DataFrame:
+    with conn:
+        c.execute('SELECT * FROM city')
+        data = c.fetchall()
+        cols = ['id', 'name', 'latitude', 'longitude', 'voivodeship', 'elevation']
+        return pd.DataFrame(data=data, columns=cols)
+    
+df = get_cities()
 
 st.map(df)
